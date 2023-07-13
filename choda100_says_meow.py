114df967f18fee6907df1177cc67055d1fbc6073
Stella_Trace <shozokuonewel@gmail.com>
2023-07-13 11:11:20 +0900

    Update choda100_says_meow.py

--- choda100_says_meow.py
+++ choda100_says_meow.py
@@ -1,154 +1,84 @@
-# This example requires the 'message_content' privileged intent to function.
-
 import asyncio
-
 import discord
-import yt_dlp as youtube_dl
-
 from discord.ext import commands
 
-# Suppress noise about console usage from errors
-youtube_dl.utils.bug_reports_message = lambda: ''
+import asyncio
+import discord
+from discord.ext import commands,tasks
+import os
+import yt_dlp as youtube_dl
 
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
 
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn',
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-
-# youtube 음악과 로컬 음악의 재생을 구별하기 위한 클래스 작성.
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-
-# 음악 재생 클래스. 커맨드 포함.
-class Music(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def join(self, ctx, *, channel: discord.VoiceChannel):
-        """Joins a voice channel"""
-
-        if ctx.voice_client is not None:
-            return await ctx.voice_client.move_to(channel)
-
-        await channel.connect()
-
-    @commands.command()
-    async def play(self, ctx, *, query):
-        """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
-
-        await ctx.send(f'Now playing: {query}')
-
-    @commands.command()
-    async def yt(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
-
-        await ctx.send(f'Now playing: {player.title}')
-
-    @commands.command()
-    async def stream(self, ctx, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
-
-        await ctx.send(f'Now playing: {player.title}')
-
-    @commands.command()
-    async def volume(self, ctx, volume: int):
-        """Changes the player's volume"""
-
-        if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
-
-        ctx.voice_client.source.volume = volume / 100
-        await ctx.send(f"Changed volume to {volume}%")
-
-    @commands.command()
-    async def stop(self, ctx):
-        """Stops and disconnects the bot from voice"""
-
-        await ctx.voice_client.disconnect()
-
-    @play.before_invoke
-    @yt.before_invoke
-    @stream.before_invoke
-    async def ensure_voice(self, ctx):
-        if ctx.voice_client is None:
-            if ctx.author.voice:
-                await ctx.author.voice.channel.connect()
-            else:
-                await ctx.send("You are not connected to a voice channel.")
-                raise commands.CommandError("Author not connected to a voice channel.")
-        elif ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("!"),
-    description='Relatively simple music bot example',
-    intents=intents,
-)
-
+game = discord.Game("해킹")
+bot = commands.Bot(command_prefix='초다 ',intents=intents, status=discord.Status.online, activity=game)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
+    print(f'We have logged in as {bot.user}')
+    
+@bot.command(aliases=['도움말'])
+async def 도움(ctx):
+    embed = discord.Embed(title="poskBot", description="설명", color=0x4432a8)
+    embed.add_field(name="정보", value="초다 정보", inline=False)
+    embed.add_field(name="유튜브채널", value="초다 유튜브채널", inline=False)
+    embed.add_field(name="또다른유튜브채널", value="초다 또다른유튜브채널", inline=False)
+    await ctx.send(embed=embed)
+    embed.add_field(name="디스코드서버", value="초다 디스코드서버", inline=False)
+    await ctx.send(embed=embed)
+
+def check(reaction, user):
+            return user == message.author
+
+@bot.command(aliases=['정보'])
+async def informationofchoda(ctx):
+    await ctx.send(f'{ctx.author.mention}, 나는 컴퓨터를 가지고 노는 고양이야.')
+
+@bot.command(aliases=['유튜브채널'])
+async def informationofchodayoutubechennel(ctx):
+    await ctx.send(f'{ctx.author.mention}, https://www.youtube.com/@choda100')
+
+@bot.command(aliases=['또다른유튜브채널'])
+async def informationofchodasecondyoutubechennel(ctx):
+    await ctx.send(f'{ctx.author.mention}, https://www.youtube.com/channel/UC1JEORrM7oPouwxoYeRKmjA')
+
+@bot.command(aliases=['디스코드서버'])
+async def informationofchodadiscordserver(ctx):
+    await ctx.send(f'{ctx.author.mention}, https://discord.com/invite/n5jfJYxwcP')
 
 
-async def main():
-    async with bot:
-        await bot.add_cog(Music(bot))
-        await bot.start('MTA5NDUxMDMxNzE2NzQ2NDQ5OA.GYqpry.t-pg0Vp1V9EDv2YoBdXK_KdnPwcKWq_Zpwh98Y')
+async def song_start(voice, url):
+    try:
+        if not voice.is_playing() and not voice.is_paused():
+            ydl_opts = {'format': 'bestaudio'}
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(f'https://www.youtube.com{url}', download=False) # 파일로 다운로드 하지 않고 재생
+                URL = info['formats'][0]['url']
+            
+            voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        #voice.play(discord.FFmpegPCMAudio(executable = './ffmpeg-4.4-full_build-shared/bin/ffmpeg.exe', source='./song.mp3'))
+
+        while voice.is_playing() or voice.is_paused():
+            await asyncio.sleep(0.1)
+    except:
+        return
+
+@bot.command(aliases = ['play', 'p', 'ㅔ'])
+async def Play(ctx,*,  keyword):
+    try:
+        results = YoutubeSearch(keyword, max_results=1).to_dict() # title과 url_suffix를 사용. 자세한 내용은 하단 링크 참고
+
+        channel = ctx.author.voice.channel
+        if bot.voice_clients == []:
+            await channel.connect()
+            #await ctx.send("connected to the voice channel, " + str(bot.voice_clients[0].channel))
+        voice = bot.voice_clients[0]
+        
+        await song_start(voice, results[0]['url_suffix'])
+    except:
+        await ctx.send("Play Error")
 
 
-asyncio.run(main())
+bot.run("MTA5NDUxMDMxNzE2NzQ2NDQ5OA.GYqpry.t-pg0Vp1V9EDv2YoBdXK_KdnPwcKWq_Zpwh98Y")
