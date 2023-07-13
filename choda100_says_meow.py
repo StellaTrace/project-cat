b78ce59d7e32ed36f349309508761a4ba0471551
Stella_Trace <shozokuonewel@gmail.com>
2023-07-13 10:50:09 +0900

    Update choda100_says_meow.py

--- choda100_says_meow.py
+++ choda100_says_meow.py
@@ -1,47 +1,12 @@
+# This example requires the 'message_content' privileged intent to function.
+
 import asyncio
+
 import discord
-from discord.ext import commands,tasks
-import os
 import yt_dlp as youtube_dl
 
-intents = discord.Intents().all()
-client = discord.Client(intents=intents)
-
-game = discord.Game("해킹")
-bot = commands.Bot(command_prefix='초다 ',intents=intents, status=discord.Status.online, activity=game)
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-    
-@bot.command(aliases=['도움말'])
-async def 도움(ctx):
-    embed = discord.Embed(title="poskBot", description="설명", color=0x4432a8)
-    embed.add_field(name="정보", value="초다 정보", inline=False)
-    embed.add_field(name="유튜브채널", value="초다 유튜브채널", inline=False)
-    embed.add_field(name="또다른유튜브채널", value="초다 또다른유튜브채널", inline=False)
-    await ctx.send(embed=embed)
-    embed.add_field(name="디스코드서버", value="초다 디스코드서버", inline=False)
-    await ctx.send(embed=embed)
-
-def check(reaction, user):
-            return user == message.author
-
-@bot.command(aliases=['정보'])
-async def informationofchoda(ctx):
-    await ctx.send(f'{ctx.author.mention}, 나는 컴퓨터를 가지고 노는 고양이야.')
-
-@bot.command(aliases=['유튜브채널'])
-async def informationofchodayoutubechennel(ctx):
-    await ctx.send(f'{ctx.author.mention}, https://www.youtube.com/@choda100')
-
-@bot.command(aliases=['또다른유튜브채널'])
-async def informationofchodasecondyoutubechennel(ctx):
-    await ctx.send(f'{ctx.author.mention}, https://www.youtube.com/channel/UC1JEORrM7oPouwxoYeRKmjA')
-
-@bot.command(aliases=['디스코드서버'])
-async def informationofchodadiscordserver(ctx):
-    await ctx.send(f'{ctx.author.mention}, https://discord.com/invite/n5jfJYxwcP')
+from discord.ext import commands
+from dico_token import Token
 
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
@@ -164,4 +129,27 @@ class Music(commands.Cog):
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
-bot.run('MTA5NDUxMDMxNzE2NzQ2NDQ5OA.GYqpry.t-pg0Vp1V9EDv2YoBdXK_KdnPwcKWq_Zpwh98Y')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    description='Relatively simple music bot example',
+    intents=intents,
+)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+async def main():
+    async with bot:
+        await bot.add_cog(Music(bot))
+        await bot.start('MTA5NDUxMDMxNzE2NzQ2NDQ5OA.GYqpry.t-pg0Vp1V9EDv2YoBdXK_KdnPwcKWq_Zpwh98Y')
+
+
+asyncio.run(main())
