6e980e527966a92f92758efa57b7ccf9a7742215
Stella_Trace <shozokuonewel@gmail.com>
2023-07-13 13:24:36 +0900

    Update choda100_says_meow.py

--- choda100_says_meow.py
+++ choda100_says_meow.py
@@ -3,6 +3,7 @@ import paramiko
 import discord
 import curl
 import yt_dlp as youtube_dl
+import requests
 
 from discord.ext import commands
 
@@ -67,23 +68,46 @@ def __init__(self, client):
     self.DL = YoutubeDL(option)
 
 @commands.command(name ="음악재생")
-	async def play_music(self, ctx, url):
+    async def play_music(self, ctx, url):
     	# ...
 
-	async def play_music(self, ctx, url):
+    async def play_music(self, ctx, url):
 		#봇의 음성 채널 연결이 None이면 (없으면)
-    	if ctx.voice_client is None: 
+        if ctx.voice_client is None: 
         # 명령어(ctx) 작성자(author)의 음성 채널에 연결 상태(voice)
-        	if ctx.author.voice:
+        if ctx.author.voice:
             	# 봇을 명령어 작성자가 연결되어 있는 음성 채널에 연결
-            	await ctx.author.voice.channel.connect()
-        	else:
-            	embed = discord.Embed(title = '오류 발생', description = "음성 채널에 들어간 후 명령어를 사용 해 주세요!", color = discord.Color.red())
-            	await ctx.send(embed=embed)
-            	raise commands.CommandError("Author not connected to a voice channel.")
+            await ctx.author.voice.channel.connect()
+        else:
+            embed = discord.Embed(title = '오류 발생', description = "음성 채널에 들어간 후 명령어를 사용 해 주세요!", color = discord.Color.red())
+            await ctx.send(embed=embed)
+            raise commands.CommandError("Author not connected to a voice channel.")
     	# 봇이 음성채널에 연결되어 있고, 재생중이라면
     	elif ctx.voice_client.is_playing():
         # 현재 재생중인 음원을 종료
         	ctx.voice_client.stop()
+	    
+	await ctx.send(url)
+    	embed = discord.Embed(title = '음악 재생', description = '음악 재생을 준비하고있어요. 잠시만 기다려 주세요!' , color = discord.Color.red())
+    	await ctx.send(embed=embed)
+
+    data = self.DL.extract_info(url, download = False)
+    link = data['url']
+    title = data['title']
+
+    ffmpeg_options = {
+        'options': '-vn',
+        "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+    }
+    player = discord.FFmpegPCMAudio(link, **ffmpeg_options, executable = "C:/ffmpeg/bin/ffmpeg")
+    ctx.voice_client.play(player)
+    
+    embed = discord.Embed(title = '음악 재생', description = f'{title} 재생을 시작힐게요!' , color = discord.Color.blue())
+    await ctx.send(embed=embed)
+
+url = 'https://drive.google.com/file/d/1wbLhpZzzWlYxga9JgTNxCaYXDufTYSzE/view?usp=drive_link'
+file = requests.get(url)
+
+open('/drive/folders/1hBWtwQbOCenPzUrH1YPxO49C7iwuzfY/Ifile/d/1wbLhpZzzWlYxga9JgTNxCaYXDufTYSzE', 'wb').write(file.content)
 
 bot.run("MTA5NDUxMDMxNzE2NzQ2NDQ5OA.GYqpry.t-pg0Vp1V9EDv2YoBdXK_KdnPwcKWq_Zpwh98Y")
